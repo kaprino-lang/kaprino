2ea4fa7428f27a54680353d70f28332d99d5deee
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-23 12:52:22 +0900

    Replace old lines with fully compiler option

--- src/interpreter/KaprinoParser.impl.cpp
+++ src/interpreter/KaprinoParser.impl.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 #include "antlr4-runtime.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/InitLLVM.h"
 
+#include "KaprinoLogger.h"
 #include "../parser/KaprinoLexer.h"
 #include "../parser/KaprinoParser.h"
 #include "StatementVisitor.h"
@@ -12,28 +24,60 @@
 
 using namespace antlr4;
 
-std::vector<StatementObject*>* ParseFile(std::string fileName);
+std::vector<StatementObject*>* ParseFile(std::string text);
 void GenerateCode(std::vector<StatementObject*>* programObj, std::string fileName);
 
-#if KAPRINO_OPTIMIZER_ON
+#ifdef KAPRINO_EMIT_OBJECT_CODE
+
+void EmitObjectCode(llvm::Module* module);
+
+#endif
+
+#ifdef KAPRINO_OPTIMIZER_ON
 
 void OptimizeAll(llvm::Module* module);
 
 #endif
 
 int main(int argc, const char* argv[]) {
+    llvm::InitLLVM X(argc, argv);
+
+    KAPRINO_LOG_INIT();
+
+    KAPRINO_LOG("Kaprino started");
+
     if (argc <= 1) {
-        std::cerr << "[ERROR] No input" << std::endl;
+        KAPRINO_ERR("No input");
         return -1;
     }
 
-    auto programObject = ParseFile(argv[1]);
+    std::string input_file_path = argv[1];
+    std::ifstream input_file(input_file_path);
 
-    GenerateCode(programObject, "code.ll");
+    if (!input_file.good()) {
+        KAPRINO_ERR("Not found input files: \"" << argv[1] << "\"");
+        return -1;
+    }
+
+    std::ostringstream ss;
+    ss << input_file.rdbuf();
+    std::string input_text;
+    input_text = ss.str();
+
+    KAPRINO_LOG("Read input files succeeded \"" << argv[1] << "\"");
+
+    auto programObject = ParseFile(input_text);
+
+    KAPRINO_LOG("Parsing succeeded");
+
+    std::string input_without_extension = input_file_path.substr(0, input_file_path.find_last_of("."));
+    std::string output_file_path = input_without_extension + ".ll";
+
+    GenerateCode(programObject, output_file_path);
 }
 
-std::vector<StatementObject*>* ParseFile(std::string fileName) {
-    ANTLRFileStream input(fileName);
+std::vector<StatementObject*>* ParseFile(std::string text) {
+    ANTLRInputStream input(text);
     KaprinoLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
     KaprinoParser parser(&tokens);
@@ -83,18 +127,75 @@ void GenerateCode(std::vector<StatementObject*>* programObj, std::string fileNam
     std::error_code errorcode;
     auto stream = new llvm::raw_fd_ostream(fileName, errorcode);
 
-    std::cout << "[LOG] Outputing file task returns: " << errorcode.value() << std::endl;
+    KAPRINO_LOG("Outputing file task: " << errorcode.message());
 
-#if KAPRINO_OPTIMIZER_ON
+#ifdef KAPRINO_OPTIMIZER_ON
 
-    GenerateCode(module);
+    OptimizeAll(module);
 
 #endif
 
     module->print(*stream, nullptr);
+
+#ifdef KAPRINO_EMIT_OBJECT_CODE
+
+    EmitObjectCode(module);
+
+#endif
 }
 
-#if KAPRINO_OPTIMIZER_ON
+#ifdef KAPRINO_EMIT_OBJECT_CODE
+
+void EmitObjectCode(llvm::Module* module) {
+    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+
+    KAPRINO_LOG("Target: " << TargetTriple);
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    std::string Error;
+    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+
+    if (!Target) {
+        KAPRINO_ERR(Error);
+        return;
+    }
+
+    auto CPU = "generic";
+    auto Features = "";
+
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
+    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+    module->setDataLayout(TargetMachine->createDataLayout());
+    module->setTargetTriple(TargetTriple);
+
+    auto Filename = "output.o";
+    std::error_code errorcode;
+    llvm::raw_fd_ostream dest(Filename, errorcode);
+
+    KAPRINO_LOG("Emit object file task: " << errorcode.message());
+
+    llvm::legacy::PassManager pass;
+    auto FileType = llvm::TargetMachine::CGFT_ObjectFile;
+
+    if (TargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
+        KAPRINO_ERR("TargetMachine can't emit a file of this type");
+        return;
+    }
+
+    pass.run(*module);
+    dest.flush();
+}
+
+#endif
+
+#ifdef KAPRINO_OPTIMIZER_ON
 
 void OptimizeAll(llvm::Module* module) {
     llvm::PassBuilder passBuilder;
