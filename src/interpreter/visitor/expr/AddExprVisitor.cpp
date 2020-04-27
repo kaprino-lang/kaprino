e42971ef561d2f2990bcaaf9318bde2f188a85f8
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-27 19:09:29 +0900

    Implement AddExprVisitor

---
+++ src/interpreter/visitor/expr/AddExprVisitor.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+#include "../../../parser/KaprinoParserBaseVisitor.h"
+#include "../../abstructs/StatementObject.h"
+#include "../../abstructs/ExprObject.h"
+#include "../../StatementVisitor.h"
+#include "../../KaprinoAccelerator.h"
+#include "../../internallib/InternalFuncs.h"
+
+class AddExprObject : ExprObject {
+   public:
+    bool isPlus;
+    ExprObject* left;
+    ExprObject* right;
+
+    virtual llvm::Value* codegen(llvm::IRBuilder<>* builder, llvm::Module* module) override {
+        auto l = left->codegen(builder, module);
+        auto r = right->codegen(builder, module);
+        if (isPlus)
+            return builder->CreateFAdd(l, r);
+        else
+            return builder->CreateFSub(l, r);
+    }
+};
+
+antlrcpp::Any StatementVisitor::visitAddExpr(KaprinoParser::AddExprContext* ctx) {
+    auto exprObj = new AddExprObject();
+
+    exprObj->left = visit(ctx->expr(0)).as<ExprObject*>();
+    exprObj->right = visit(ctx->expr(1)).as<ExprObject*>();
+    exprObj->isPlus = ctx->add_op()->getText() == "+";
+
+    return (ExprObject*)exprObj;
+}
