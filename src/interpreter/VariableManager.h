b6ad510d392cf5ecbdc46b422b11654d7f048cec
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-29 14:54:15 +0900

    Add Scope

--- src/interpreter/VariableManager.h
+++ src/interpreter/VariableManager.h
@@ -5,12 +5,19 @@
 
 #include "KaprinoAccelerator.h"
 
+struct ParamInfo {
+    std::string name;
+    llvm::AllocaInst* alloca_ptr;
+};
+
 class VariableManager {
    public:
-    static bool exists(std::string paramName);
     static void create(llvm::IRBuilder<>* builder, llvm::Module* module, std::string paramName, llvm::AllocaInst* allocated);
     static llvm::Value* getptr(llvm::IRBuilder<>* builder, llvm::Module* module, std::string paramName);
     static void store(llvm::IRBuilder<>* builder, llvm::Module* module, std::string paramName, llvm::Value* value);
+    static void add_scope();
+    static void remove_scope();
    private:
-    static std::unordered_map<std::string, llvm::AllocaInst*> params;
+    static int depth;
+    static std::vector<ParamInfo> params;
 };
