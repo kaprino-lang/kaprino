c873ed21ec809a9d5363e687dec62dcd358fc6d1
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-09 17:34:50 +0900

    Add a system to compile object code

--- src/interpreter/ExecutableGenerator.h
+++ src/interpreter/ExecutableGenerator.h
@@ -4,6 +4,8 @@
 
 void EmitLLVMIR(llvm::Module* module, bool optimize);
 
+std::string EmitObjectCode(llvm::Module* module, bool optimize);
+
 void EmitExecutable(llvm::Module* module, bool optimize);
 
 void Interpret(llvm::Module* module, bool optimize);
