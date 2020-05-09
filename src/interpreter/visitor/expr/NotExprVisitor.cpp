8ef78896733351f17705910f78c748dfd62b6f7f
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-09 15:57:51 +0900

    Implement not-op

---
+++ src/interpreter/visitor/expr/NotExprVisitor.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include "../../../parser/KaprinoParserBaseVisitor.h"
+#include "../../abstructs/ExprObject.h"
+#include "../../abstructs/StatementObject.h"
+#include "../../KaprinoAccelerator.h"
+#include "../../StatementVisitor.h"
+
+class NotExprObject : ExprObject {
+   public:
+    ExprObject* value;
+
+    virtual llvm::Value* codegen(llvm::IRBuilder<>* builder, llvm::Module* module) override {
+        return builder->CreateNot(value->codegen(builder, module));
+    }
+};
+
+antlrcpp::Any StatementVisitor::visitNotExpr(KaprinoParser::NotExprContext* ctx) {
+    auto exprObj = new NotExprObject();
+
+    exprObj->value = visit(ctx->expr()).as<ExprObject*>();
+
+    return (ExprObject*)exprObj;
+}
