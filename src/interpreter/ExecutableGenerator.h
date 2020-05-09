f446aa43eacb0c6f2349602c2ae5dd5d84b002e9
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-09 18:43:49 +0900

    Delete interpret mode

--- src/interpreter/ExecutableGenerator.h
+++ src/interpreter/ExecutableGenerator.h
@@ -7,5 +7,3 @@ void EmitLLVMIR(llvm::Module* module, bool optimize);
 std::string EmitObjectCode(llvm::Module* module, bool optimize);
 
 void EmitExecutable(llvm::Module* module, bool optimize);
-
-void Interpret(llvm::Module* module, bool optimize);
