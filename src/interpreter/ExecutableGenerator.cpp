ee13e36c212cc1208905b55a43b8fc44a507817a
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-15 18:53:58 +0900

    Implement the system using bitcode

--- src/interpreter/ExecutableGenerator.cpp
+++ src/interpreter/ExecutableGenerator.cpp
@@ -16,67 +16,26 @@ void EmitLLVMIR(llvm::Module* module, bool optimize) {
     KAPRINO_LOG("Outputing file task: " << errorcode.message());
 }
 
-std::string EmitObjectCode(llvm::Module* module, bool optimize) {
+std::string EmitBitcode(llvm::Module* module, bool optimize) {
     auto llvmir_path = module->getName().str();
 
-#if _WIN32
-
-    auto objectcode_path = KAPRINO_RM_FILE_EXT(llvmir_path) + ".obj";
-
-#else
-
-    auto objectcode_path = KAPRINO_RM_FILE_EXT(llvmir_path) + ".o";
-
-#endif
-
-    remove(objectcode_path.c_str());
-
-    auto target_triple = llvm::sys::getDefaultTargetTriple();
-    module->setTargetTriple(target_triple);
-
-    std::string errormsg;
-    auto target = llvm::TargetRegistry::lookupTarget(target_triple, errormsg);
-
-    if (!target) {
-        KAPRINO_ERR(errormsg);
-        throw -1;
-    }
-
-    auto CPU = "generic";
-    auto Features = "";
-
-    llvm::TargetOptions opt;
-    auto RM = llvm::Optional<llvm::Reloc::Model>();
-    auto target_machine = target->createTargetMachine(
-        target_triple, CPU, Features, opt, RM, llvm::None,
-        optimize ? llvm::CodeGenOpt::Aggressive : llvm::CodeGenOpt::Default
-    );
-
-    module->setDataLayout(target_machine->createDataLayout());
+    auto bitcode_path = KAPRINO_RM_FILE_EXT(llvmir_path) + ".bc";
 
     std::error_code error_code;
-    llvm::raw_fd_ostream dest(objectcode_path, error_code, llvm::sys::fs::OF_None);
+    llvm::raw_fd_ostream dest(bitcode_path, error_code, llvm::sys::fs::OF_None);
+
+    llvm::WriteBitcodeToFile(*module, dest);
 
     if (error_code) {
-        KAPRINO_ERR("Object code generation faield: " << error_code);
+        KAPRINO_ERR("Bitcode generation faield: " << error_code);
         throw -1;
     }
 
-    llvm::legacy::PassManager pass;
-
-    if (target_machine->addPassesToEmitFile(pass, dest, nullptr, llvm::TargetMachine::CodeGenFileType::CGFT_ObjectFile)) {
-        KAPRINO_ERR("Object code generation faield");
-        throw -1;
-    }
-
-    pass.run(*module);
-    dest.flush();
-
-    return objectcode_path;
+     return bitcode_path;
 }
 
 std::string EmitExecutable(llvm::Module* module, bool optimize) {
-    auto objectcode_path = EmitObjectCode(module, optimize);
+    auto bitcode_path = EmitBitcode(module, optimize);
 
 #if _WIN32
 
@@ -99,7 +58,7 @@ std::string EmitExecutable(llvm::Module* module, bool optimize) {
 #endif
 
     compile_command << " ";
-    compile_command << objectcode_path;
+    compile_command << bitcode_path;
 
     KAPRINO_LOG("Execute external tool: " << compile_command.str());
 
--- src/interpreter/ExecutableGenerator.h
+++ src/interpreter/ExecutableGenerator.h
@@ -4,6 +4,6 @@
 
 void EmitLLVMIR(llvm::Module* module, bool optimize);
 
-std::string EmitObjectCode(llvm::Module* module, bool optimize);
+std::string EmitBitcode(llvm::Module* module, bool optimize);
 
 std::string EmitExecutable(llvm::Module* module, bool optimize);
