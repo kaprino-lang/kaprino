4cebd64388974a3a0a2084cecf2ddf0b306b6030
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-18 20:05:05 +0900

    Cast char ptr to string

--- src/interpreter/KaprinoAccelerator.h
+++ src/interpreter/KaprinoAccelerator.h
@@ -78,3 +78,9 @@
 //
 
 #define KAPRINO_RM_FILE_EXT(file_path) (file_path).substr(0, (file_path).find_last_of("."))
+
+//
+// ------ Cast ------
+//
+
+#define KAPRINO_TO_STR(ptr) [](char* p) { std::string str(p); return str; }(ptr)
