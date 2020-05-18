a335a16d18399dbd42a5a1948c8f6843d8247650
Akihisa Yagi <capra314cabra@gmail.com>
2020-05-18 20:07:12 +0900

    Add PackagePathManager

---
+++ src/interpreter/PackagePathManager.h
@@ -0,0 +1,25 @@
+#include "KaprinoAccelerator.h"
+
+class PackagePathManager {
+   public:
+    PackagePathManager(std::string package_name);
+    std::string find();
+
+   private:
+    std::string package_name;
+    const std::string KAPRINO_PATH_ENV = "KAPRINOPKG";
+
+#if _WIN32
+
+    const std::string SPLIT_CHAR = ";";
+
+#else
+
+    const std::string SPLIT_CHAR = ":";
+
+#endif
+
+    std::string checkdirs(std::string env_name);
+    std::vector<std::string> getdirs(std::string env);
+    inline bool exists (std::string name);
+};
