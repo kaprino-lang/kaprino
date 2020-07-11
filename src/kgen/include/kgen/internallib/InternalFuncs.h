dd2729c4c20e4d851e2c53a103ec2d6cac00f984
Akihisa Yagi <capra314cabra@gmail.com>
2020-07-11 22:30:02 +0900

    Fix failing to link InternalFuncs

--- src/kgen/include/kgen/internallib/InternalFuncs.h
+++ src/kgen/include/kgen/internallib/InternalFuncs.h
@@ -3,8 +3,12 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 
+namespace kaprino::kgen {
+
 llvm::Function* get_printf(llvm::IRBuilder<>* builder, llvm::Module* module);
 
 llvm::Function* get_scanf(llvm::IRBuilder<>* builder, llvm::Module* module);
 
 llvm::Function* get_pow(llvm::IRBuilder<>* builder, llvm::Module* module);
+
+};  // namespace kaprino::kgen
