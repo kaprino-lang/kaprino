552e7b925d713a8f7718d8c2b35f10f4efd98cfe
Akihisa Yagi <capra314cabra@gmail.com>
2020-07-11 21:14:00 +0900

    Delete codes which were already moved to kgen

--- src/kprc/visitor/cstatements/CStatementObject.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../KaprinoAccelerator.h"
-#include "ClassStructureObject.h"
-
-class CStatementObject {
-   public:
-    virtual void codegen(llvm::IRBuilder<>* builder, llvm::Module* module, ClassStructureObject* members) { }
-};
--- src/kprc/visitor/statements/StatementObject.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "llvm/IR/IRBuilder.h"
-
-class StatementObject {
-   public:
-    virtual void codegen(llvm::IRBuilder<>* builder, llvm::Module* module) { }
-};
