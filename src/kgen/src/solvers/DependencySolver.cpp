7b741241275c3f954efced7c645e62136b289522
Akihisa Yagi <capra314cabra@gmail.com>
2020-07-25 22:57:20 +0900

    Reimplement DependencySolver

--- src/kgen/src/solvers/DependencySolver.cpp
+++ src/kgen/src/solvers/DependencySolver.cpp
@@ -8,8 +8,32 @@ using namespace antlr4;
 
 namespace kaprino::kgen {
 
-std::vector<StatementObject*>* DependencySolver::parseFile(std::string text) {
-    ANTLRInputStream input(text);
+DependencySolver depsolver;
+
+DependencySolver::DependencySolver() {
+    auto env_dirs = processEnv(KAPRINO_PATH_ENV);
+    searchDirectories.insert(
+        searchDirectories.end(),
+        env_dirs->begin(),
+        env_dirs->end()
+    );
+
+    env_dirs = processEnv("PATH");
+    searchDirectories.insert(
+        searchDirectories.end(),
+        env_dirs->begin(),
+        env_dirs->end()
+    );
+}
+
+std::vector<StatementObject*>* DependencySolver::parseFile(std::string file_path) {
+    logger->asrt(
+        std::filesystem::exists(file_path),
+        "Not found an input file: \"" + file_path + "\"",
+        "internal", 0, 0
+    );
+
+    ANTLRFileStream input(file_path);
     KaprinoLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
     KaprinoParser parser(&tokens);
@@ -39,11 +63,12 @@ std::vector<StatementObject*>* DependencySolver::importRequirePackage(std::strin
     }
 
     auto file_name = findPackageFile(require_package);
-    statements = readFile(file_name);
+    statements = parseFile(file_name);
 
     auto llvmir_name = std::filesystem::path(file_name)
         .replace_extension(".impl.ll")
         .string();
+
     if (std::filesystem::exists(llvmir_name)) {
         linkFiles.push_back(llvmir_name);
     }
@@ -51,103 +76,59 @@ std::vector<StatementObject*>* DependencySolver::importRequirePackage(std::strin
 }
 
 std::string DependencySolver::findPackageFile(std::string package_name) {
-    auto executable_file = current_file;
-
-    auto file = std::filesystem::path(executable_file)
-        .parent_path()
-        .append(package_name + ".kpr")
-        .string();
-    if (std::filesystem::exists(file)) {
-        logger->log(
-            "Package found: " + file,
-            "internal",
-            0,
-            0
-        );
-        return file;
-    }
-
-    file = checkdirs(package_name, KAPRINO_PATH_ENV);
-    if (file != "") {
-        return file;
-    }
-
-    file = checkdirs(package_name, "PATH");
-    if (file != "") {
-        return file;
+    for (auto importDirectory : searchDirectories) {
+        auto package = findPackageFile(importDirectory, package_name);
+        if (package != "") {
+            return package;
+        }
     }
 
     logger->error(
         "Not found the package: " + package_name,
-        "internal",
-        0,
-        0
+        "internal", 0, 0
     );
     throw -1;
 }
 
-std::vector<std::string> DependencySolver::linkFiles;
-//    *(new std::vector<std::string>());
+std::string DependencySolver::findPackageFile(std::string directory_name, std::string package_name) {
+    auto file = directory_name + "/" + package_name + ".kpr";
 
-std::vector<StatementObject*>* DependencySolver::readFile(std::string path) {
-    std::ifstream input_file(path);
-
-    std::ostringstream ss;
-    ss << input_file.rdbuf();
-    std::string input_text;
-    input_text = ss.str();
-
-    return parseFile(input_text);
+    if (std::filesystem::exists(file)) {
+        logger->log(
+            "Package found: " + file
+        );
+        return file;
+    }
+    else {
+        return "";
+    }
 }
 
-std::string DependencySolver::checkdirs(std::string package_name, std::string env_name) {
+std::vector<std::string>* DependencySolver::processEnv(std::string env_name) {
     auto envcstr = std::getenv(env_name.c_str());
 
     if (envcstr == nullptr) {
         logger->warn(
             "The environment variable, " + env_name + " is not setted",
-            "internal",
-            0,
-            0
+            "internal", 0, 0
         );
-        return "";
+        return nullptr;
     }
 
-    auto env = KAPRINO_TO_STR(envcstr);
+    auto pathes = new std::vector<std::string>();
+    std::string env(envcstr);
 
-    auto envdirs = getdirs(env);
-    for (auto dir : envdirs) {
-        auto file = dir + "/" + package_name + ".kpr";
-        if (std::filesystem::exists(file)) {
-            logger->log(
-                "Package found: " + file,
-                "internal",
-                0,
-                0
-            );
-            return file;
-        }
-    }
-
-    return "";
-}
-
-std::vector<std::string> DependencySolver::getdirs(std::string env) {
-    std::vector<std::string> pathes;
     if (env.find(KAPRINO_ENV_SPLIT_CHAR) == std::string::npos) {
-        pathes.push_back(env);
+        pathes->push_back(env);
         return pathes;
     }
 
     size_t pos = 0;
     while ((pos = env.find(KAPRINO_ENV_SPLIT_CHAR)) != std::string::npos) {
-        pathes.push_back(env.substr(0, pos));
+        pathes->push_back(env.substr(0, pos));
         env.erase(0, pos + 1);
     }
     return pathes;
 }
-
-std::vector<std::string> DependencySolver::alreadyImported;
-//    *(new std::vector<std::string>());;
 
 };  // namespace kaprino::kgen
