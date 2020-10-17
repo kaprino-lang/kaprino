ffae43c05d750fc6b2f2bc65994de1f45a95eaa0
Akihisa Yagi <capra314cabra@gmail.com>
2020-10-17 22:09:15 +0900

    Update bootstrap.sh

--- configure.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-git clone --depth 1 --branch 2020.07 https://github.com/microsoft/vcpkg.git
-.\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg install antlr4 llvm[target-all]
-mkdir build
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
