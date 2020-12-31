a43a33901849b833840e1c51cf415fac38a398c9
Akihisa Yagi <capra314cabra@gmail.com>
2020-12-31 19:08:30 +0900

    Remove meaningless match expressions

--- src/kgen/src/functions/external_function.rs
+++ src/kgen/src/functions/external_function.rs
@@ -38,16 +38,11 @@ impl<'ctx> ExternalFunction {
     }
 
     pub fn codegen(&self, gen: &CodeGen<'ctx>) -> Result<(), String> {
-        match self.get_func_type(gen) {
-            Ok(func_type) => {
-                gen.module.add_function(&self.get_info().name, func_type, Some(Linkage::External));
+        let func_type = self.get_func_type(gen)?;
 
-                Ok(())
-            },
-            Err(error_message) => {
-                Err(error_message)
-            }
-        }
+        gen.module.add_function(&self.get_info().name, func_type, Some(Linkage::External));
+
+        Ok(())
     }
 }
 
